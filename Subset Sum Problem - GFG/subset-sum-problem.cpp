02840c80561ba6ff8ef2682897b9704da5442805
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-06-04 21:05:28 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -8,22 +8,33 @@ using namespace std;
 //User function template for C++
 
 class Solution{   
-private:
-    bool function(int ind, vector<int>& arr, int sum, vector<vector<int>>&dp){
-        if(sum == 0) return true;
-        if(ind == 0) return (arr[0] == sum);
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        bool notTake = function(ind-1, arr, sum, dp);
-        bool take = false;
-        if(arr[ind] <= sum) take = function(ind-1, arr, sum-arr[ind], dp);
-        return dp[ind][sum]=take || notTake;
-    }
+// private:
+//     bool function(int ind, vector<int>& arr, int sum, vector<vector<int>>&dp){
+//         if(sum == 0) return true;
+//         if(ind == 0) return (arr[0] == sum);
+//         if(dp[ind][sum] != -1) return dp[ind][sum];
+//         bool notTake = function(ind-1, arr, sum, dp);
+//         bool take = false;
+//         if(arr[ind] <= sum) take = function(ind-1, arr, sum-arr[ind], dp);
+//         return dp[ind][sum]=take || notTake;
+//     }
 public:
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
         int n = arr.size();
-        vector<vector<int>>dp(n, vector<int>(sum+1, -1));
-        return function(n-1, arr, sum, dp);
+        vector<vector<bool>>dp(n, vector<bool>(sum+1, 0));
+        for(int i=0; i<n; i++) dp[i][0]=true;
+        if(arr[0]  <= sum) dp[0][arr[0]] = true;
+        
+        for(int ind=1; ind<n; ind++){
+            for(int target = 1; target<=sum; target++){
+                bool notTake = dp[ind-1][target];
+                bool take = false;
+                if(arr[ind] <= target) take = dp[ind-1][target-arr[ind]];
+                dp[ind][target]=take || notTake; 
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 
