ec4110c0d5ed52de9e6470c86dec5f62576ad992
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-09-06 02:15:59 +0530

    Time: 3 ms (96.88%) | Memory: 11.2 MB (88.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -15,18 +15,34 @@ public:
 */
 
 class Solution {
-private:
-unordered_map<Node*, Node*>visited;
 public:
     Node* copyRandomList(Node* head) {
-        if(head == NULL) return NULL;
-        if(visited.find(head) != visited.end()) return visited[head];
-        
-        Node* node = new Node(head->val, NULL, NULL);
-        visited[head]=node;
-        
-        node->next = copyRandomList(head->next);
-        node->random = copyRandomList(head->random);
-        return node;
+        Node* itr=head;
+        Node* fr=head;
+        while(itr!=NULL){
+            fr = itr->next;
+            Node* copy = new Node(itr->val);
+            itr->next = copy;
+            copy->next = fr;
+            itr = fr;
+        }
+
+        itr = head;
+        while(itr){
+            if(itr->random) itr->next->random = itr->random->next;
+            itr = itr->next->next;
+        }
+
+        itr = head;
+        Node* dumy = new Node(0);
+        Node* copy = dumy;
+        while(itr){
+            fr=itr->next->next;
+            copy->next = itr->next;
+            itr->next = fr;
+            copy = copy->next;
+            itr = fr;
+        }
+        return dumy->next;
     }
 };
