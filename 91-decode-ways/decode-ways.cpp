593c3a64551045b133f708d6542aa7e6092d9bdc
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-09-07 14:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (78.40%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,18 +1,25 @@
 class Solution {
-    vector<int> dp;
-    int help(int i, string s){
-        if(i>=s.size()) return 1;
-        else if(s[i]=='0') return 0;
-        else if(i==s.size()-1) return 1;
-        else if(dp[i]!=-1) return dp[i];
-        else if(s[i]=='1' || (s[i]=='2' && (s[i+1]>='0'&& s[i+1]<='6')))
-            return dp[i]=help(i+1, s) + help(i+2, s);
-        else return dp[i]=help(i+1, s);
-    }
+    // vector<int> dp;
+    // int help(int i, string s){
+    //     if(i>=s.size()) return 1;
+    //     else if(s[i]=='0') return 0;
+    //     else if(i==s.size()-1) return 1;
+    //     else if(dp[i]!=-1) return dp[i];
+    //     else if(s[i]=='1' || (s[i]=='2' && (s[i+1]>='0'&& s[i+1]<='6')))
+    //         return dp[i]=help(i+1, s) + help(i+2, s);
+    //     else return dp[i]=help(i+1, s);
+    // }
 public:
     int numDecodings(string s) {
-        dp.resize(s.length());
-        dp.assign(s.length(), -1);
-        return help(0, s);
+        int n = s.size();
+        vector<int>dp(s.size()+1);
+        if(s.size()==0 || s[0]=='0')return 0;
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(s[i-1] != '0') dp[i]=dp[i-1];
+            if(s[i-2]=='1')dp[i] += dp[i-2];
+            else if(s[i-2]=='2' && (s[i-1]>='0' && s[i-1]<='6')) dp[i]+=dp[i-2];
+        }
+        return dp[n];
     }
 };
