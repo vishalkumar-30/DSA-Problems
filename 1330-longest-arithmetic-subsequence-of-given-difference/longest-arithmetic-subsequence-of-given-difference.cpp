bc6aacc800832f0f57d31c8492e8de6430d92228
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-09-12 17:59:12 +0530

    Time: 119 ms (91.93%) | Memory: 55 MB (47.00%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,14 +1,24 @@
 class Solution {
+// private:
+//     int solve(int ind, int prev, int diff, vector<int> &nums, vector<vector<int>> &dp){
+//         if(ind == nums.size()) return 0;
+//         if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
+//         int notTake = solve(ind+1, prev, diff, nums, dp);
+//         int take = 0;
+//         if(prev == -1 || nums[ind]-nums[prev] == diff){
+//             take = 1 + solve(ind + 1, ind, diff, nums, dp);
+//         }
+//         return dp[ind][prev+1]=max(take, notTake);
+//     }
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int, int>dp;
-        int ans = 0;
-        for(int i=0; i<arr.size(); i++){
-            int temp = arr[i]-difference;
-            int tempAns = 0;
-            if(dp.count(temp)) tempAns = dp[temp];
-            dp[arr[i]]=1 + tempAns;
-            ans = max(ans, dp[arr[i]]);
+        int n = arr.size();
+        unordered_map<int, int> dp;
+        int ans = 1;
+        for(int a : arr){
+            int prev = dp.count(a-difference)?dp[a-difference]:0;
+            dp[a] = prev+1;
+            ans = max(ans, dp[a]);
         }
         return ans;
     }
