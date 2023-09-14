8b6a6a24542daa07efd33fcc1d5a8075a95a9ad5
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-09-14 21:49:53 +0530

    Time: 39 ms (77.11%) | Memory: 15.2 MB (75.07%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,30 +1,24 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> visited(m, vector<vector<int>>(n, vector<int>(k + 1, INT_MAX)));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, 0, 0, k});
-        visited[0][0][k] = 0;
-        while (!pq.empty()) {
-            auto cur = pq.top(); pq.pop();
-            int d = cur[0], x = cur[1], y = cur[2], rest = cur[3];
-            if (x == m - 1 && y == n - 1) return d;
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                int nrest = rest - grid[nx][ny];
-                if (nrest >= 0 && d + 1 < visited[nx][ny][nrest]) {
-                    visited[nx][ny][nrest] = d + 1;
-                    pq.push({d + 1, nx, ny, nrest});
-                }
+        int ans=-1, n=0, dir[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        vector<vector<int>> vis(grid.size(), vector<int>(grid[0].size(), -1));
+        queue<array<int, 3>> q;
+        q.push({0, 0, k});
+        while(!q.empty()){
+            if(n-- == 0){
+                ans++;
+                n = q.size()-1;
             }
+            auto [x, y, obj] = q.front();
+            q.pop();
+            if(x+1 == grid.size() && y+1 == grid[0].size()) return ans;
+            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()) continue;
+            obj -= grid[x][y];
+            if(vis[x][y] >= obj) continue;
+            vis[x][y] = obj;
+            for(auto d:dir) q.push({x+d[0], y+d[1], obj});
         }
         return -1;
     }
-private:
-    vector<int> dx{0, -1, 0, 1}, dy{-1, 0, 1, 0};
 };
