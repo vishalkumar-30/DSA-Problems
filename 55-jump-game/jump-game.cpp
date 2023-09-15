a2a1225f94eee406ee437da251c68a79ecc1691b
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-09-15 09:17:11 +0530

    Time: 56 ms (43.44%) | Memory: 48.7 MB (20.44%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int minJump=0;
+        for(int i=nums.size()-2; i>=0; i--){
+            minJump++;
+            if(nums[i]>=minJump) minJump=0;
+        }
+        if(minJump == 0) return true;
+        else return false;
+    }
+};
