0413d255609312c8b5bca80a2bf8b1ea592d3080
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2023-11-29 10:56:03 +0530

    Time: 92 ms (55.35%) | Memory: 87.6 MB (49.23%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n);
-        vector<int> prefixSum(n), suffixSum(n);
-        
-        // Calculate and initialize the prefix sums & suffixSum array
-        prefixSum[0] = nums[0];
-        suffixSum[n - 1] = nums[n - 1];
-        
-        // Calculate the prefix sums & suffixSum array in one loop
-        for (int i = 1; i < n; ++i) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-            suffixSum[n - i - 1] = suffixSum[n - i] + nums[n - i - 1];
+        int totalSum=0;
+        vector<int>ans;
+        for(auto it: nums) totalSum += it;
+        int leftSum=0;
+        for(int i=0; i<nums.size(); i++){
+            int temp = ((nums[i]*i)-leftSum) + ((totalSum-leftSum)-(nums[i]*(nums.size()-i)));
+            ans.push_back(temp);
+            leftSum += nums[i];
         }
-        
-        // Calculate absolute differences and update the result array
-        for (int i = 0; i < n; ++i) {
-            int currentAbsoluteDiff = ((nums[i] * i) - prefixSum[i]) + (suffixSum[i] - (nums[i] * (n - i - 1)));
-            result[i] = currentAbsoluteDiff;
-        }
-        
-        return result;
+        return ans;
     }
 };
