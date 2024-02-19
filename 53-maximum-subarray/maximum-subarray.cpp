17fddd57ff5a94b85e5919eab78dd39409868238
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-02-19 23:28:58 +0530

    Time: 86 ms (40.27%) | Memory: 70.2 MB (36.76%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currMax=0;   //train 1
-        int maxi=INT_MIN;  // train 2
-// ab ye train me bogi purani judna chahti hai ya phir nayi bogi
+        int maxi = INT_MIN;
+        int curr = 0;
         for(int i=0; i<nums.size(); i++){
-            if(currMax<0){
-                currMax = nums[i];
+            if(curr<0){
+                curr = nums[i];
             }
             else{
-                currMax += nums[i];
+                curr += nums[i];
             }
-            maxi = max(maxi, currMax);
+            maxi = max(maxi, curr);
         }
         return maxi;
     }
