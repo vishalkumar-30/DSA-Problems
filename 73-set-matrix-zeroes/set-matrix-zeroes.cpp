134b9fc294a0ab4dc22c3f3e79a1edddb5ac5691
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-02-19 19:20:59 +0530

    Time: 5 ms (92.49%) | Memory: 16.1 MB (37.25%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,22 +3,21 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int>col(n, 0);
-        vector<int>row(m, 0);
+        int temp = 1;
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            if(matrix[i][0]==0) temp=0;
+            for(int j=1; j<n; j++){
                 if(matrix[i][j]==0){
-                    col[j]=1;
-                    row[i]=1;
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(row[i]==1) matrix[i][j]=0;
-                if(col[j]==1) matrix[i][j]=0;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>0; j--){
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
             }
-        }        
-        // return matrix;
+            if(temp == 0) matrix[i][0] = 0;
+        }      
     }
 };
