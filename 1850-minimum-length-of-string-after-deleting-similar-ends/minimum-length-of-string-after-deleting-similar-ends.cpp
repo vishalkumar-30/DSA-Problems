e10b2e74f7f4c04c413b76fa434611de7d8c9d2c
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-03-05 18:02:59 +0530

    Time: 26 ms (55.86%) | Memory: 14 MB (44.14%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -4,7 +4,7 @@ public:
         int l=0, r=s.size()-1;
         while(l<=r){
             if(s[l]!=s[r] || r==l) return r-l+1;
-            if(s[l] == s[r]){
+            else{
                 char t = s[l];
                 while(s[l]==t && l<=r) l++;
                 while(t==s[r] && l<=r) r--;
