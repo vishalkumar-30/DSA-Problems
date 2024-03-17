efc4642e1acb81cec46e6199445fe932a1819ff5
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-03-17 18:33:21 +0530

    Time: 566 ms (79.56%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -4,11 +4,5 @@ FROM Employee e1
 INNER JOIN Employee e2
 ON e1.id = e2.managerId
 group by e1.id
-HAVING count(e2.managerId)>=5;
+HAVING count(e1.id)>=5;
 
-
--- SELECT e.name
--- FROM Employee e ,Employee m
--- WHERE e.id = m.managerId 
--- GROUP BY e.id
--- HAVING COUNT(m.id) >= 5
