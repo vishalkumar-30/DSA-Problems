93c600b1503a9d7fe54b53a842cfe286d0fc50dc
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-03-20 20:43:08 +0530

    Time: 6 ms (69.45%) | Memory: 9.4 MB (96.21%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int helper(int i, int j, string s1, string s2, vector<vector<int>> &dp){
-        if(i==0) return j;
-        else if(j==0) return i;
-        if(dp[i-1][j-1] != -1) return dp[i-1][j-1];
-        if(s1[i-1] == s2[j-1]) return dp[i-1][j-1]=helper(i-1, j-1, s1, s2, dp);
-        else return dp[i-1][j-1]=min(
-            1 + helper(i-1, j, s1, s2, dp),   //deletion
-            min(1 + helper(i, j-1, s1, s2, dp),   //replace
-            1 + helper(i-1, j-1, s1, s2, dp))  //Insert
-        );
-    }
+    // int helper(int i, int j, string s1, string s2, vector<vector<int>> &dp){
+    //     if(i==0) return j;
+    //     else if(j==0) return i;
+    //     if(dp[i-1][j-1] != -1) return dp[i-1][j-1];
+    //     if(s1[i-1] == s2[j-1]) return dp[i-1][j-1]=helper(i-1, j-1, s1, s2, dp);
+    //     else return dp[i-1][j-1]=min(
+    //         1 + helper(i-1, j, s1, s2, dp),   //deletion
+    //         min(1 + helper(i, j-1, s1, s2, dp),   //replace
+    //         1 + helper(i-1, j-1, s1, s2, dp))  //Insert
+    //     );
+    // }
     int minDistance(string s1, string s2) {
         int n1 = s1.length(), n2 = s2.length();
-        vector<vector<int>>dp(n1+1, vector<int>(n2+1, -1));
-        for(int i=0; i<=n1; i++) dp[i][0] = i;
-        for(int j=0; j<=n2; j++) dp[0][j] = j;
+        vector<int>prev(n2+1, 0), curr(n2 +1, 0);
+        // for(int i=0; i<=n1; i++) dp[i][0] = i;
+        for(int j=0; j<=n2; j++) prev[j] = j;
 
         for(int i=1; i<=n1; i++){
+            curr[0]=i;
             for(int j=1; j<=n2; j++){
-                if(s1[i-1] == s2[j-1]) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=min(
-                    1 + dp[i-1][j],   //deletion
-                    min(1 + dp[i][j-1],   //replace
-                    1 + dp[i-1][j-1])  //Insert
+                if(s1[i-1] == s2[j-1]) curr[j]=prev[j-1];
+                else curr[j]=min(
+                    1 + prev[j],   //deletion
+                    min(1 + curr[j-1],   //replace
+                    1 + prev[j-1])  //Insert
                 );
             }
+            prev = curr;
         }
 
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
