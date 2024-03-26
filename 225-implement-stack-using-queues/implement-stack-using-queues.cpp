2a01e9e71616e44bf755dcdc9f7e867ee72d858f
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-03-27 00:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (75.63%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,37 +1,52 @@
+#include <queue>
+
+using namespace std;
+
 class MyStack {
 public:
-    queue<int> q1, q2;
-    int top_element;
+    queue<int> q1;
 
     MyStack() {
-        top_element = -1;
+        
     }
 
     void push(int x) {
         q1.push(x);
-        top_element = x; // Update top element
     }
 
     int pop() {
         if (q1.empty())
             return -1;  // Stack is empty
 
-        while (q1.size() > 1) {
-            top_element = q1.front(); // Update top element while moving elements
-            q2.push(q1.front());
+        int size = q1.size();
+        for (int i = 0; i < size - 1; ++i) {
+            int x = q1.front();
             q1.pop();
+            q1.push(x);
         }
 
         int x = q1.front();
         q1.pop();
 
-        swap(q1, q2); // Swap q1 and q2 to maintain the stack property
-
         return x;
     }
 
     int top() {
-        return top_element;
+        if (q1.empty())
+            return -1;  // Stack is empty
+
+        int size = q1.size();
+        for (int i = 0; i < size - 1; ++i) {
+            int x = q1.front();
+            q1.pop();
+            q1.push(x);
+        }
+
+        int x = q1.front();
+        q1.pop();
+        q1.push(x); // Push the top element back to maintain the order
+
+        return x;
     }
 
     bool empty() {
