d8e3269389d832a673d28bf4bbf70a1e2b41eb11
Vishal Kumar <102590210+vishalkumar-30@users.noreply.github.com>
2024-04-01 19:08:01 +0530

    Time: 21 ms (20.10%) | Memory: 19.7 MB (51.97%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -12,9 +12,23 @@ class Solution {
         return dp[ind][amount] = T+nT;
     }
 public:
-    int change(int amount, vector<int>& coins) {
+    int change(int amp, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        return helper(n-1, coins, amount, dp);
+        vector<vector<int>>dp(n, vector<int>(amp+1, 0));
+
+        for(int i=0; i<=amp; i++) {
+            if(i%coins[0] == 0) dp[0][i] = 1;
+        }
+        for(int ind=1; ind<n; ind++){
+            for(int amount=0; amount<=amp; amount++){
+                int nT = dp[ind-1][amount];
+                int T = 0;
+                if(coins[ind] <= amount){
+                    T = dp[ind][amount-coins[ind]];
+                }
+                dp[ind][amount] = T+nT;
+            }
+        }
+        return dp[n-1][amp];
     }
 };
